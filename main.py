2a8ff9f1d6c5b4f944deb69647a9b00d4061de93
Damian Monea <damianmonea981109@gmail.com>
2020-06-09 15:22:41 +0300

    Added Danutza

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ profile_face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_profilef
 
 # recognizer = cv2.face.LBPHFaceRecognizer_create(neighbors = 8)
 
-vadim_mode = True
+vadim_mode = False
 vadim_location = "./vadim/"
-vadim_files = ["ordine.mp3", "oglinda.mp3"]
+
+danutza_mode = True
+danutza_location = "./dana/"
 language = 1 # 0 - english; 1 - romanian
 insults = {}
+
 if language == 0:
     with open("insults_en.json", "r") as insultFile:
         insults = json.load(insultFile)
@@ -26,11 +29,8 @@ elif language == 1:
     with open("insults_ro.json", "r") as insultFile:
         insults = json.load(insultFile)
 
-thanks1 = "Am văzut că v-a plăcut ursulețul cu insulte, deci acest tik tok este prezentat de el."
-thanks2 = " Vă multumesc pentru 20 de mii de urmăritori. Mă bucur că încă vă plac videoclipurile pe care le postez."
-thanks3 = " Sunteti cei mai tari!"
 other_insults = insults["other"]
-insult_time_delta = 8
+insult_time_delta = 20
 TTFI = int(insult_time_delta * 0.75) # Time to first insult
 last_insult_timestamp = int(datetime.now().timestamp()) - TTFI
 smallest_insult_count = 0
@@ -50,21 +50,6 @@ except cv2.error as e:
 def getTimeStamp():
     return int(datetime.now().timestamp())
 
-def mergeDetections(frontal, profile):
-    faces = []
-    for (xf, yf, wf, hf) in frontal:
-        if len(profile) == 0:
-            faces.append(((xf, yf, wf, hf)))
-        for (xp, yp, wp, hp) in profile:
-            f_x_ctr = xf + (wf / 2)
-            f_y_ctr = yf + (hf / 2)
-            p_x_ctr = xp + (wp / 2)
-            p_y_ctr = yp + (hp / 2)
-            dist = sqrt((f_x_ctr - p_x_ctr)**2 + (f_y_ctr - p_y_ctr)**2)
-            if (dist < 15):
-                faces.append((xf, yf, wf, hf))
-    return faces
-
 def detectFaceAndDrawRect(frame, recognizer):
     global frontal_face_cascade
     global profile_face_cascade
@@ -76,7 +61,7 @@ def detectFaceAndDrawRect(frame, recognizer):
     for (x, y, w, h) in frontals:
         roi_gray = gray[y:y+h, x:x+w]
         roi = frame[y:y+h, x:x+w]
-
+        face_frame = frame[y:y+h, x:x+w]
         #recognize faces in ROI
         # id_, conf = recognizer.predict(roi_gray)
         # if conf >= 45:
@@ -89,7 +74,7 @@ def detectFaceAndDrawRect(frame, recognizer):
         color = (255, 0, 0) #BGR, not RGB
         stroke = 2
         cv2.rectangle(frame, (x, y), (x + w, y + h), color, stroke)
-    return (frame, len(frontals))
+    return (frame, len(frontals), [])
 
 def getRandRange(insultSet):
     global smallest_insult_count
@@ -117,28 +102,44 @@ def insult(insultSet):
     insult = insultSet[current_idx]["insult"]
     insultSet[current_idx]["count"] += 1
     filename = "temp.mp3"
-    # tts = gTTS(insult, lang='en')
-    # tts.save(filename)
-    # playsound(filename)
-    # os.remove(filename)
-    engine.say(thanks1)
-    engine.runAndWait()
-    engine.say(thanks2)
-    engine.runAndWait()
-    engine.say(thanks3)
-    engine.runAndWait()
+    tts = gTTS(insult, lang='en')
+    try:
+        os.remove(filename)
+    except OSError as err:
+        pass
+    tts.save(filename)
+    playsound(filename, False)
 
-aux = 0
-def pitica_nenorocita():
-    global aux
-    #idx = randrange(int((len(vadim_files) * 20) % len(vadim_files)) + 1)
-    idx = aux
-    aux += 1
-    if (idx < 2):
-        selected_file = vadim_location + vadim_files[idx]
-        playsound(selected_file)
+def load_vadim():
+    global insults
+    global vadim_location
+    insults["vadim"] = []
+    for root, dirs, files in os.walk(vadim_location):
+        for name in files:
+            insults["vadim"].append({"filename": vadim_location + name, "count": 0})
+
+def load_danutza():
+    global insults
+    global danutza_location
+    insults["danutza"] = []
+    for root, dirs, files in os.walk(danutza_location):
+        for name in files:
+            insults["danutza"].append({"filename": danutza_location + name, "count": 0})
+
+def vadim_tudor():
+    global insults
+    rand_nr = randrange(len(insults["vadim"]))
+    playsound(insults["vadim"][rand_nr]["filename"], False)
+
+def dana_budeanu():
+    global insults
+    rand_nr = randrange(len(insults["danutza"]))
+    playsound(insults["danutza"][rand_nr]["filename"], False)
 
 if __name__ == "__main__":
+
+    collect_face_data = True
+    face_name = "damian"
 
     lang_name = ""
     if language == 0:
@@ -149,23 +150,44 @@ if __name__ == "__main__":
     voices = engine.getProperty('voices') 
     engine.setProperty('voice', lang_name)
 
+    if vadim_mode:
+        load_vadim()
+    if danutza_mode:
+        load_danutza()
+
+    turn = 0
+
     while(True):
         ret, frame = cap.read()
 
         # Detect and recognize faces
-        (processed_frame, nr_faces) = detectFaceAndDrawRect(frame, 0)
+        (processed_frame, nr_faces, faces) = detectFaceAndDrawRect(frame, 0)
 
         cv2.imshow('frame', processed_frame)
         c = cv2.waitKey(20)
         if (getTimeStamp() - last_insult_timestamp >= insult_time_delta) and nr_faces > 0:
             last_insult_timestamp = getTimeStamp()
-            if not vadim_mode:
+            if not vadim_mode and not danutza_mode:
                 insult(other_insults)
             else:
-                pitica_nenorocita()
+                rand_nr = randrange(1000)
+                if rand_nr % 2 == 0 and vadim_mode:
+                    if turn >= 2 and danutza_mode:
+                        dana_budeanu()
+                        turn -= 1
+                        continue
+                    vadim_tudor()
+                    turn += 1
+                else:
+                    if turn <= -2 and vadim_mode:
+                        vadim_tudor()
+                        turn += 1
+                        continue
+                    dana_budeanu()
+                    turn -= 1
         if c & 0xFF == ord('q'):
             break
-        if c & 0xFF == ord('s'):
+        if c & 0xFF == ord('s') and collect_face_data:
             cv2.imwrite("capture.jpg", processed_frame)
 
     cap.release()
